62a9e4e6208ffe3f6396670581d6d3b00ad05ae3
Tarun <92459398+mckinsy@users.noreply.github.com>
2024-01-16 12:00:04 +0530

    Update DangerousPattern.java

--- DangerousPattern.java
+++ DangerousPattern.java
@@ -5,33 +5,15 @@ public class DangerousPattern
 	public void pattern(int n)
 	{ 
 		int k=n;
-//		int[][] ar=new int [n*2-1][n*2-1];
-		while(n>=1)
-		{
-			for(int i=0;i<n*2-1;i++)
-			{
-				for(int j=0;j<n*2-1;j++)
-				{
-					if((i==0||i==n*2-2)||(j==0||j==n*2-2))
-					System.out.print(n+" ");
-					else
-					System.out.print("  ");
-					//		for(int i=0;i<=n*2-2;i++)
-					//		{ 
-					//			for(int j=n*2-2;j>=0;j--)
-					//			{     
-					//				int diff;
-					//				diff=i<j?i:j;
-					//				diff=diff<(n*2-2)-j?diff:(n*2-2)-j;
-					//				diff=diff<(n*2-2)-i?diff:(n*2-2)-i; 
-					//				System.out.print(n-diff+" ");
-				}
-				System.out.println();
-				n--;
-			}
-			//n--;
-			k=n;
-		}
+							for(int i=0;i<=n*2-2;i++)
+							{ 
+								for(int j=n*2-2;j>=0;j--)
+								{     
+									int diff;
+									diff=i<j?i:j;
+									diff=diff<(n*2-2)-j?diff:(n*2-2)-j;
+									diff=diff<(n*2-2)-i?diff:(n*2-2)-i; 
+									System.out.print(n-diff+" ");
 	}
 	public static void main(String[] args)
 	{
