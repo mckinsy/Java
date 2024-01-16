1155d981eeacceb29b3804fc7145f93464ec39ab
Tarun <92459398+mckinsy@users.noreply.github.com>
2024-01-16 11:58:06 +0530

    Update MinCoinsChange.java

--- MinCoinsChange.java
+++ MinCoinsChange.java
@@ -27,11 +27,4 @@ public class MinCoinsChange
 		        sc.close();
 		    }
 		    
-		} // be decent look , be serious no unnecessary laugh even very very scarce laugh, be serious and most impressive unique
-         //i am not the one who try to excell world is so big to beat others i've to work more hard and think more effectively and optimistic also optimized precise 
-      ///be ready for such situation where maximum fails nd you excell there keep working 
-     // kbhi kisi ko poora gyan mt do khuli kitaab mt bno jb bhi kisi se milo usse direct high level knowledge mt do usski level of understanding ko prkho sbse basic sbse km cheez use btaao vha se start kro phir aage tk le jaao agr vo smjh le to vrna khud aaage bdo use choro vhi nipta do topic khtm
-     // e very very curious and exciting to learn new and quick also accurate
-   ///i want to become user friendly programs, fun to use,easy to understand, simple and most super smart also creative innovative revolutionary and impressive ,real heroic, solid player,attractive which make others jaw drops and impress them jai shree krishna
-//desired to be the bestest of all so be underground and work continuously improve yourself to excellent in world international level excellence
-//speak through actions rather than speaking everything also be man of your word also keep rocking keep shocking, what's next till lead the world in excellence and beyond that like google so no one can think to compete
+		}
